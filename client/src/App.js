3e3f7e0407f2f99533546dbf12c61cf3fa71b9e2
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-20 09:57:57 -0600

    fixed path for private route, stuff is showing up!

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        <PrivateRoute exact path="/colors" component={BubblePage} />
+        <PrivateRoute exact path="/bubbles" component={BubblePage} />
       </div>
     </Router>
   );
